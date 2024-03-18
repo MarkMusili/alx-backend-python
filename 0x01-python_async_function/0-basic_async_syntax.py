32f15efb7bbec9d66bb6203fd1ffbad62058dc0c
MarkMusili <musiliyrn@gmail.com>
2024-03-18 19:14:35 +0300

    Version 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic asynchronous task
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Co-routine that take in max_delay
+    Returns the max_delay using random module
+    """
+    return random.uniform(0, max_delay)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Measure the runtime
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int = 10) -> float:
+    """
+    Measure the runtime
+    Returns a float of the runtime / n
+    """
+    start = time.time()
+    await wait_n(n, max_delay)
+    end = time.time()
+    return (end - start) / n
