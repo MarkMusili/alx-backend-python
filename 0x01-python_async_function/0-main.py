3c467a61c4842c3497efe4ba855c46c8e19162ee
MarkMusili <musiliyrn@gmail.com>
2024-03-19 19:09:47 +0300

    Version 1

--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -7,3 +7,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 print(asyncio.run(wait_random()))
 print(asyncio.run(wait_random(5)))
 print(asyncio.run(wait_random(15)))
+print(type(asyncio.run(wait_random(23))))
